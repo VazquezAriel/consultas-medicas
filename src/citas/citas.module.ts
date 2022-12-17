cc8d40f5e526d6a50f34fb615b96ab7111a23ddc
Jhon14DEA <jfarezv@est.ups.edu.ec>
2022-12-17 17:11:51 -0500

    Correccion de errores en modulos

--- src/citas/citas.module.ts
+++ src/citas/citas.module.ts
@@ -11,7 +11,6 @@ import { Cita } from './entities/cita.entity';
   providers: [CitasService],
   imports:[
     PacientesModule,
-    MedicosModule,
     TypeOrmModule.forFeature([Cita])
   ]
 })
--- src/medicos/medicos.module.ts
+++ src/medicos/medicos.module.ts
@@ -8,7 +8,6 @@ import { EspecialidadesModule } from '../especialidades/especialidades.module';
 @Module({
   controllers: [MedicosController],
   providers: [MedicosService],
-  exports: [MedicosService],
   imports: [
     EspecialidadesModule,
     TypeOrmModule.forFeature([Medico])
