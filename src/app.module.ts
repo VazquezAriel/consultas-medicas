4952a242baf31c9c7d8ae171817ab2dd2968468e
bryambepz <brryamparra2709@gmail.com>
2023-01-24 00:10:24 -0500

    CORS para la conexión con el angular y pgadmin en docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { EspecialidadesModule } from './especialidades/especialidades.module';
 import { ConsultasModule } from './consultas/consultas.module';
 import { CitasModule } from './citas/citas.module';
 import { TransaccionesModule } from './transacciones/transacciones.module';
+import postgres from 'postgres';
 
 @Module({
   imports: [
@@ -17,11 +18,11 @@ import { TransaccionesModule } from './transacciones/transacciones.module';
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'postgres',
-        host: process.env.DB_HOST,
-        port: +process.env.DB_PORT,
-        database: process.env.DB_NAME,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
+        host: 'localhost',
+        port: 5432,
+        database: 'consultas-medicasDB',
+        username: 'postgres',
+        password: 'root',
         autoLoadEntities: true,
         synchronize: true,
       })
--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: ['http://localhost:4200'],
+    methods: ['POST', 'PATCH', 'DELETE', 'GET']
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true
-    })
-  )
-
+      forbidNonWhitelisted: true,      
+    }),
+  ) 
   await app.listen(3000);
 }
-bootstrap();
+bootstrap(); 
